b559b3d14dd76282aae0060e8e346d8976c1d63f
vidhurraj147@gmail.com <vidhurraj147@gmail.com>
2018-06-07 22:25:14 -0400

    THIS IS HellWorld java programs

---
+++ HellWorld2.java
@@ -0,0 +1,8 @@
+public class HellWorld2{
+
+	public static void main(String args[]){
+		System.out.println("THIS IS HELLO WORLD 2 GEDIT MADE SOME CAHNGES ");
+		System.out.println("THIS IS HELLO WORLD GEDIT");
+	}
+
+}
